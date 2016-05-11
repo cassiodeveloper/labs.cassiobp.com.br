cd3d37034c47f32937b740b9f2bc873df7fbf074
cassiodeveloper <cassio@cassiobp.com.br>
2016-05-11 15:14:07 -0300

    Initial commit

---
+++ ReadPDFText/ReadPDFText/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+namespace ReadPDFText
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PDFParser pdfParser = new PDFParser();
+
+            string fileNameOut = @"C:\Users\mtzcpd1053\Downloads\Code\80586.txt";
+
+            if (pdfParser.ExtractText(@"C:\Users\mtzcpd1053\Downloads\Code\80586.pdf", fileNameOut))
+            {
+                Console.WriteLine("Texto extraído");
+                Console.WriteLine(fileNameOut);
+            }
+            else
+                Console.WriteLine("Erro");
+            
+            Console.Read();
+        }
+    }
+}
